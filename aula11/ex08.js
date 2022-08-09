77b8d84ff1b05e0b0543dcbcc07d4474956d141f
GambollGabriel <mgdiasfernandes@gmail.com>
2022-08-09 09:01:26 -0300

    Initial commit

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`a velosidade do seu carro é de ${vel} kl/h`)
+ if (vel > 60 ){
+     console.log(`voceutrapassou a velocidade pemitida . MUTADODO`)
+ }else{
+    console.log(`dirija sempre usandoocinto de segurança`)
+       }
